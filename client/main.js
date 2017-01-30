0b7b9bd2a752fa4f0d7b9a9791ec93ae10080992
Filip Slatinac <fslat092@uottawa.ca>
2017-01-29 22:48:14 -0500

    added login route

--- client/main.js
+++ client/main.js
@@ -3,3 +3,19 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+
+Router.configure({
+    layoutTemplate: 'main'
+});
+
+Router.route('/', function () {
+  this.render('lostFound');
+});
+
+Router.route('/login', function () {
+  this.render('login');
+});
+
+Router.route('/signup', function () {
+  this.render('signup');
+});
--- server/main.js
+++ server/main.js
@@ -3,3 +3,5 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+
